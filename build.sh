40a4a97987df5c6930bdb7cc9309576f0886eb26
erolmutlu <mutlu@erolmutlu.com>
2013-11-22 20:45:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -141,7 +141,9 @@ fi
 mkdir -p .repo/local_manifests
 rm -f .repo/local_manifest.xml
 
-
+rm -rf $WORKSPACE/build_env
+git clone https://github.com/CyanogenMod/cm_build_config.git $WORKSPACE/build_env -b master
+check_result "Bootstrap failed"
 
 if [ -f $WORKSPACE/build_env/bootstrap.sh ]
 then
