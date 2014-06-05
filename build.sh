b9c4013dc68cdf920035bc97e154cff05dcbd731
erolmutlu <mutlu@erolmutlu.com>
2014-06-05 09:20:34 +0300

    back to cm list

--- build.sh
+++ build.sh
@@ -126,7 +126,7 @@ fi
 
 rm -rf .repo/manifests*
 rm -f .repo/local_manifests/dyn-*.xml
-repo init -u $SYNC_PROTO://github.com/erolmutlu/android.git -b $SYNC_BRANCH $MANIFEST
+repo init -u $SYNC_PROTO://github.com/CyanogenMod/android.git -b $SYNC_BRANCH $MANIFEST
 check_result "repo init failed."
 
 # make sure ccache is in PATH
